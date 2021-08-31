50afdb31fafbb4a68a250792b99090eebd34ad9f
HangaWilfried <wilfriedhanga5@gmail.com>
2021-08-31 17:01:55 +0100

    ajout des buttons download et play et retouches des listes au niveau du css

--- main.js
+++ main.js
@@ -6,15 +6,21 @@ const musicAvailable = document.querySelector('#musiqueDisponible');
 function getMusicApi(){
     if(searchingMethode.length !== 0){
         let maStar = searchingMethode.value;
-        let url = `https://itunes.apple.com/search?term=${maStar}&limit=5&entity=musicVideo`;
+        let url = `https://itunes.apple.com/search?term=${maStar}&limit=30&entity=musicVideo`;
         fetch(url).then(response =>response.json().then(data => {
             let displayed = '<ul>';
             for(information in data.results){
-                displayed += `<li><strong>${data.results[information].artistName}</strong> - ${data.results[information].trackCensoredName}</li>`
+                displayed += `<li><strong>${data.results[information].artistName}</strong> - ${data.results[information].trackCensoredName}
+                                <ul>
+                                    <li class="sublist"> <button>Download</button> <button>Play</button> </li>
+                                </ul>
+                             </li>`
             }
             displayed += '</ul>';
             musicAvailable.innerHTML = displayed;
-        })).catch(err => `${err} n'existe pas dans la liste`);
+        })).catch(err => {
+            musicAvailable.innerHTML = `${err} n'existe pas dans la liste`;
+        });
     }
 }
 
